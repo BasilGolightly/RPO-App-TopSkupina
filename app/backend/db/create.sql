47342ffbe26abad9d4d0a9c2b2c015b63eb557fb
MarkSadnik <basilgolightly14530@gmail.com>
2025-12-22 12:13:16 +0100

    Quick fix na SQL skripti

--- app/backend/db/create.sql
+++ app/backend/db/create.sql
@@ -1,55 +1,71 @@
-CREATE OR REPLACE DATABASE BitBug;
+DROP DATABASE IF EXISTS BitBug;
+CREATE DATABASE BitBug;
 USE BitBug;
 
 -- USER
-CREATE OR REPLACE TABLE user(
+DROP TABLE IF EXISTS users;
+CREATE TABLE users(
     id int AUTO_INCREMENT PRIMARY KEY,
     username char(30) NOT NULL,
     password char(255) NOT NULL,
     role enum('user', 'admin', 'mod') NOT NULL DEFAULT 'user',
-    joined date DEFAULT CURRENT_DATE()
+    joined date DEFAULT CURRENT_DATE
 );
 
 -- LOGIN - logiranje prijav
-CREATE OR REPLACE TABLE login(
+DROP TABLE IF EXISTS login;
+CREATE TABLE login(
     id int AUTO_INCREMENT PRIMARY KEY,
-    id_user int FOREIGN KEY REFERENCES user(id) NOT NULL,
-    logged_in timestamp DEFAULT CURRENT_TIMESTAMP()
+    id_user int NOT NULL,
+    logged_in timestamp DEFAULT CURRENT_TIMESTAMP(),
+    FOREIGN KEY (id_user) REFERENCES users(id)
 );
 
 -- POST - objava
-CREATE OR REPLACE TABLE post(
+DROP TABLE IF EXISTS post;
+CREATE TABLE post(
     id int AUTO_INCREMENT PRIMARY KEY,
-    id_user int FOREIGN KEY REFERENCES user(id) NOT NULL,
+    id_user int NOT NULL,
     title char(100) NOT NULL,
-    content text NOT NULL
-)
+    content text NOT NULL,
+    FOREIGN KEY (id_user) REFERENCES users(id)
+);
 
 -- UPLOAD - objavljena datoteka
-CREATE OR REPLACE TABLE upload(
+DROP TABLE IF EXISTS upload;
+CREATE TABLE upload(
     id int AUTO_INCREMENT PRIMARY KEY,
-    id_user int FOREIGN KEY REFERENCES user(id) NOT NULL,
-    -- id_post int FOREIGN KEY REFERENCES post(id)
+    id_user int NOT NULL,
     filename char(100) NOT NULL,
     extension char(10) NOT NULL,
-    category enum('picture', 'video', 'code', 'archive') NOT NULL
-)
+    category enum('picture', 'video', 'code', 'archive') NOT NULL,
+    FOREIGN KEY (id_user) REFERENCES users(id)
+);
 
 -- POST_UPLOAD - kateremu postu pripadajo datoteke (neobvezna relacija)
-CREATE OR REPLACE TABLE post_upload(
+DROP TABLE IF EXISTS post_upload;
+CREATE TABLE post_upload(
     id int AUTO_INCREMENT PRIMARY KEY,
-    id_post int FOREIGN KEY REFERENCES post(id) NOT NULL,
-    id_upload int FOREIGN KEY REFERENCES upload(id) NOT NULL
-)
+    id_post int NOT NULL,
+    id_upload int NOT NULL,
+    FOREIGN KEY (id_post) REFERENCES post(id),
+    FOREIGN KEY (id_upload) REFERENCES upload(id)
+);
 
 -- COMMENT - komentarji na poste / threade
-CREATE OR REPLACE TABLE comment(
+DROP TABLE IF EXISTS comment;
+CREATE TABLE comment(
     id int AUTO_INCREMENT PRIMARY KEY,
-    id_user int FOREIGN KEY REFERENCES user(id) NOT NULL,
-    id_post int FOREIGN KEY REFERENCES post(id) NOT NULL,
+    id_user int NOT NULL,
+    id_post int NOT NULL,
     content TEXT NOT NULL,
-    id_comment int FOREIGN KEY REFERENCES comment(id) DEFAULT NULL -- za reply-je na drug comment
-)
+    id_comment int DEFAULT NULL /* za reply-je na drug comment */,
+    FOREIGN KEY (id_user) REFERENCES users(id),
+    FOREIGN KEY (id_post) REFERENCES post(id),
+    FOREIGN KEY (id_comment) REFERENCES comment(id)
+);
+
+INSERT INTO users(id, username, password, role, joined) VALUES (DEFAULT, 'admin', 'root123', 'admin', DEFAULT);
 
 
 
