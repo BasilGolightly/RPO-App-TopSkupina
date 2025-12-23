3f9172673a9d9809c742d3d69d00be9ea96c211d
Wizertex <39441188+Wizertex@users.noreply.github.com>
2025-12-23 15:57:28 +0100

    začetna osnova indexa

---
+++ app/nav.php
@@ -0,0 +1,16 @@
+<link rel="stylesheet" href="./style/nav.css" />
+
+<nav>
+    <div class="navLevo">
+        <img class="logo" src="./media/logo1Pixel.png" alt="FileBug logo" />
+        <ul>
+            <li class="selected"><a href="">Domov</a></li>
+            <li><a href="">Profil</a></li>
+        </ul>
+    </div>
+    <div class="navDesno">
+        <button style="display: none">Prijava</button>
+        <h1>Janez Novak</h1>
+        <button>Odjava</button>
+    </div>
+</nav>
