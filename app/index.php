97e3401baa879b31f5eca384dc636d432ccae096
MarkSadnik <basilgolightly14530@gmail.com>
2025-12-28 19:43:57 +0100

    Globalni CSS template

--- app/index.php
+++ app/index.php
@@ -5,8 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./style/style.css" />
     <link rel="stylesheet" href="./style/index.css" />
-    
-    
     <title>BitBug</title>
 </head>
 <body>
