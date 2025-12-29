b8447c6930e933aad799d2e6c3f76482b6f4714f
Wizertex <39441188+Wizertex@users.noreply.github.com>
2025-12-29 18:46:43 +0100

    nav bar, objave, boards, footer

--- app/footer.php
+++ app/footer.php
@@ -1,5 +1,27 @@
 <link rel="stylesheet" href="./style/footer.css" />
 
 <footer>
-    <h1>&copy BitBug 2026</h1>
+    <div class="footer-top">
+        <div class="footer-left">
+            <h1>BitBug</h1>
+            <img src="./media/logo1Pixel.png" alt="logo">
+            <p><strong>FERI, RIT 2 UN, RPO</strong></p>
+            <p>2025/26</p>
+        </div>
+        <div class="footer-right">
+            <ul>
+                <p><strong>Profile</strong></p>
+                <li><a href="">My profile</a></li>
+                <li><a href="">Add post</a></li>
+                <li><a href="">Friends</a></li>
+                <li><a href="">My boards</a></li>
+            </ul>
+            <ul>
+                <p><strong>Help</strong></p>
+                <li><a href="">Documentation</a></li>
+                <li><a href="">Help</a></li>
+            </ul>
+        </div>
+    </div>
+    <p>&copy BitBug 2026</p>
 </footer>
--- app/index.php
+++ app/index.php
@@ -16,19 +16,56 @@
         ?>
         <main>
             <div id="seznam-vrh">
-                <input id="seznam-search" placeholder="Išči" type="text">
-                <button>Išči</button>
+                <input class="index-filter-checkbox" id="index-filter-objave-checkbox" type="checkbox">
+                <label class="index-filter-label" for="index-filter-objave-checkbox">Recent posts ☰</label>
+                <div class="index-filter-div" id="index-filter-boards-div">
+                    <ul>
+                        <li><button>Recent</button></li>
+                        <li><button>Oldest</button></li>
+                    </ul>
+                </div>
             </div>
             <div id="seznam">
                 <a class="objava">
                     <img src="./media/logo1Pixel.png" alt="logo">
                     <h1>Naslov objave</h1>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do </p>
+                    <div id="objava-zvezdice">★★★☆☆</div>
+                    <div id="objava-info">
+                        <div id="objava-info-levo">
+                            <p>1M views</p>
+                            <p>#coding, +1 file</p>
+                        </div>
+                        <div id="objava-info-desno">
+                            <p>Janez Novak</p>
+                            <img src="./media/logo1Pixel.png" alt="logo">
+                        </div>
+                    </div>
                 </a>
+            </div>
+
+            <div id="seznam-vrh">
+                <input class="index-filter-checkbox" id="index-filter-boards-checkbox" type="checkbox">
+                <label class="index-filter-label" for="index-filter-boards-checkbox">Boards ☰</label>
+                <div class="index-filter-div" id="index-filter-boards-div">
+                    <ul>
+                        <li><button>Recent</button></li>
+                        <li><button>Oldest</button></li>
+                    </ul>
+                </div>
+            </div>
+            <div id="seznam">
                 <a class="objava">
                     <img src="./media/logo1Pixel.png" alt="logo">
-                    <h1>Naslov objave</h1>
+                    <br>
+                    <p>#coding, +1 file</p>
+                    <h1>Naslov boarda</h1>
+                    <br>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do </p>
+                    <br>
+                    <div class="objava-board-follow">
+                        <button class="objava-board-follow-button">Follow</button>
+                    </div>
                 </a>
             </div>
         </main>
