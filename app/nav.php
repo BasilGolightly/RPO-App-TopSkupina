3c33dcbc33bc2be32539e88fd4c7e2dce03a2195
Wizertex <39441188+Wizertex@users.noreply.github.com>
2025-12-29 13:19:21 +0100

    nav bar: dodan search, spremembe

--- app/nav.php
+++ app/nav.php
@@ -4,13 +4,23 @@
     <div class="navLevo">
         <img class="logo" src="./media/logo1Pixel.png" alt="FileBug logo" />
         <ul>
-            <li class="selected"><a href="">Domov</a></li>
-            <li><a href="">Profil</a></li>
+            <li class="selected"><a href="">Home</a></li>
+            <li><a href="">Friends</a></li>
+            <li><a href="">My Profile</a></li>
+            <li><a href="">Boards</a></li>
+            <li><a href="" id="nav-add-post">Add post</a></li>
         </ul>
     </div>
     <div class="navDesno">
+        <div id="nav-search">
+            <input id="seznam-search" placeholder="Search" type="text">
+            <button>🔍</button>
+        </div>
         <button style="display: none">Prijava</button>
-        <h1>Janez Novak</h1>
-        <button>Odjava</button>
+        <div class="nav-uporabnik">
+            <h1 style="font-size: 1.2rem;"><a href="">Janez Novak</a></h1>
+            <a id="nav-logout-button" title="Logout">➜]</a>
+        </div>
+        <img class="logo" src="./media/logo1Pixel.png" alt="Uporabniska slika" />
     </div>
 </nav>
