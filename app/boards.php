bc4f5f0d10b1b42bed8bdc0c02a94ef7df69af94
LjubiteljKode474 <144672230+LjubiteljKode474@users.noreply.github.com>
2026-01-03 21:01:35 +0100

    css za create board
    cas za php

--- app/boards.php
+++ app/boards.php
@@ -1,8 +1,8 @@
 <?php
     session_start();
-    if(!isset($_SESSION['user_id']) || !isset($_SESSION['username']) || !isset($_SESSION['role'])){
+    /*if(!isset($_SESSION['user_id']) || !isset($_SESSION['username']) || !isset($_SESSION['role'])){
         header("Location: login.php");
-    }
+    }*/
 ?>
 
 <!DOCTYPE html>
@@ -11,7 +11,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./style/style.css" />
-    <link rel="stylesheet" href="./style/index.css" />
+    <link rel="stylesheet" href="./style/boards.css" />
+    <script src="backend/js/boardsScript.js" defer></script>
     <title>Boards</title>
 </head>
 <body>
@@ -20,6 +21,15 @@
             include "nav.php";
         ?>
         <main>
+            <div id="ustvari-nov">
+                <?php if (
+                    isset($_SESSION['user_id'], $_SESSION['username'], $_SESSION['role'])
+                ): ?>
+                        <button id="novboard" >Create a new board <span class="plus">+</span></button>
+                <?php else: ?>
+                        To create a board please, <a href="login.php">Login</a>
+                <?php endif; ?>
+            </div>
             <div id="seznam-vrh">
                 <input class="index-filter-checkbox" id="index-filter-boards-checkbox" type="checkbox">
                 <label class="index-filter-label" for="index-filter-boards-checkbox">Boards ☰</label>
@@ -68,6 +78,26 @@
                         <button class="objava-board-follow-button">FOLLOW</button>
                     </div>
                 </a>
+            </div>
+            <div id="boardobrazec">
+                <form method="post" action="backend/php/createBoard.php" id="bObrazec">
+                    <div class="title-wrap">
+                        <span id="pencil">✎</span>
+                        <h1 id="naslov">CREATE A NEW BOARD</h1>
+                    </div>
+                    <br><br>
+                    <div class="input-wrap">
+                        <input type="text" placeholder="Naslov" required>
+                        <br>
+                        <input type="text" placeholder="Tags">
+                        <br>
+                        <textarea name="description" id="description" rows="4" placeholder="Description"></textarea>
+                    </div>
+                    <div class="submit-wrap">
+                        <input id="submitBoard" type="submit" value="CREATE BOARD">
+                        <button id="cancelBoard" type="button">cancel</button>
+                    </div>
+                </form>
             </div>
         </main>
         <?php
