b3198ec42acc3221378392f1132feda23c20439d
MarkSadnik <basilgolightly14530@gmail.com>
2026-01-12 18:50:00 +0100

    Update conn.php

--- app/backend/php/conn.php
+++ app/backend/php/conn.php
@@ -1,8 +1,14 @@
 <?php
+/* ZA DOCKER
 $sname = getenv("DB_HOST");
 $uname = getenv("DB_USER");
 $password = getenv("DB_PASSWORD");
 $db_name = getenv("DB_NAME");
+*/
+$sname = "localhost";
+$uname = "root";
+$password = "";
+$db_name = "BitBug";
 
 $conn = new mysqli($sname, $uname, $password, $db_name);
 
